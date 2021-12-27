9d98351d472a6c026cf2e8d27335ba2e54aa91ac
ranasevdenur <91938167+ranasevdenur@users.noreply.github.com>
2021-12-28 00:12:23 +0300

    Update Calculator.js

--- Project/Calculator.js
+++ Project/Calculator.js
@@ -6,23 +6,23 @@ function removeZero() {
       var value = document.getElementById("output").innerHTML;
       if (value == "0") {
            value = " "
-           document.getElementById("output").innerHTML = value;
+           document.getElementById("output").innerHTML=value;
       }
 }
 function perc() {
       removeZero()
       var value = document.getElementById("output").innerHTML;
       value = value / 100;
-      document.getElementById("output").innerHTML = value;
+      document.getElementById("output").innerHTML=value;
 }
 
 function fordisplay(value) {
       removeZero()
-      document.getElementById("output").innerHTML += value;
+      document.getElementById("output").innerHTML+=value;
 }
 function solve() {
      removeZero()
      var equation = document.getElementById("output").innerHTML;
      var solved = eval(equation);
-     document.getElementById('output').innerHTML = solved;
+     document.getElementById('output').innerHTML=solved;
 }
